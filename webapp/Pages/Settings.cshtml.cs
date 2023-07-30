070d45de43f7f76b21ee1a27c99832c8ac5b1c00
Kevin Doolaeghe <doolaeghe.kevin@hotmail.com>
2023-07-30 03:14:04 +0200

    Started to add localization texts fr/en. Updated UI: Added filter settings Updated media query for nav and dashboard: 992px Added blank settings page

---
+++ webapp/Pages/Settings.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages {
+
+    public class SettingsModel : PageModel {
+
+        public void OnGet() { }
+    }
+}
